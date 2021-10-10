cd0089aaee90b8b0fe25ee5a780dce0637d05229
Dima <sushkevichd@gmail.com>
2021-10-10 18:07:03 +0300

    small fix

--- internal/infra/db/migrations/1633811316_create_trees_table.up.sql
+++ internal/infra/db/migrations/1633811316_create_trees_table.up.sql
@@ -1,5 +1,5 @@
 create table trees(
-    id uuid default uuid_generate_v4() constraint users_pk primary key,
+    id uuid default uuid_generate_v4() constraint trees_pk primary key,
     created_at timestamptz default current_timestamp,
     name varchar(100),
     description varchar(1000)
