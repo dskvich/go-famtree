5f605f58530825432b5f04ad254a5efbd6be0e9c
Dima <sushkevichd@gmail.com>
2021-10-24 11:05:38 +0300

    small update for graceful shutdown

--- internal/infra/httpserver/builder.go
+++ internal/infra/httpserver/builder.go
@@ -68,11 +68,11 @@ func (b *builder) Start() {
 	}()
 	log.Infof("http server started on port: %s", b.port)
 
-	interrupt := make(chan os.Signal, 1)
-	signal.Notify(interrupt, os.Interrupt, syscall.SIGTERM)
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
 
-	<-interrupt
-	log.Info("http server is shutting down...")
+	sig := <-c
+	log.Infof("got '%s' signal, http server is shutting down...", sig)
 
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
